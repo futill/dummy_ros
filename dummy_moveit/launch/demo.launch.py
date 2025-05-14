5a313bc3e353523b2148f9a6e4bbfd023483ba13
futill <137772411+futill@users.noreply.github.com>
2025-05-14 15:53:57 +0800

    Add files via upload

---
+++ dummy_moveit/launch/demo.launch.py
@@ -0,0 +1,238 @@
+import os
+import yaml
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition, UnlessCondition
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+from ament_index_python.packages import get_package_share_directory
+import xacro
+
+
+def load_file(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+
+    try:
+        with open(absolute_file_path, "r") as file:
+            return file.read()
+    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
+        return None
+
+
+def load_yaml(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+
+    try:
+        with open(absolute_file_path, "r") as file:
+            return yaml.safe_load(file)
+    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
+        return None
+
+
+def generate_launch_description():
+
+    # planning_context
+    robot_description_config = xacro.process_file(
+        os.path.join(
+            get_package_share_directory("dummy_moveit"),
+            "config",
+            "dummy2-SLDASM.urdf.xacro",
+        )
+    )
+    robot_description = {"robot_description": robot_description_config.toxml()}
+
+    robot_description_semantic_config = xacro.process_file(
+        os.path.join(
+            get_package_share_directory("dummy_moveit"),
+            "config",
+            "dummy2-SLDASM.srdf",
+        )
+    )
+
+    robot_description_semantic = {
+        "robot_description_semantic": robot_description_semantic_config.toxml()
+    }
+
+    joint_limits_yaml = load_yaml(
+        "dummy_moveit", "config/joint_limits.yaml"
+    )
+    pilz_cartesian_limits_yaml = load_yaml(
+        "dummy_moveit", "config/pilz_cartesian_limits.yaml"
+    )
+    robot_description_planning = {
+        "robot_description_planning": {
+            **joint_limits_yaml,
+            **pilz_cartesian_limits_yaml,
+        }
+    }
+    kinematics_yaml = load_yaml(
+        "dummy_moveit", "config/kinematics.yaml"
+    )
+    robot_description_kinematics = {"robot_description_kinematics": kinematics_yaml}
+
+    # Planning Functionality
+    planning_pipelines_config = {
+        "default_planning_pipeline": "ompl",
+        "planning_pipelines": ["pilz", "ompl"],
+        "pilz": {
+            "planning_plugin": "pilz_industrial_motion_planner/CommandPlanner",
+            "request_adapters": "",
+            "start_state_max_bounds_error": 0.1,
+        },
+        "ompl": {
+            "planning_plugin": "ompl_interface/OMPLPlanner",
+            "request_adapters": """default_planner_request_adapters/AddTimeOptimalParameterization default_planner_request_adapters/FixWorkspaceBounds default_planner_request_adapters/FixStartStateBounds default_planner_request_adapters/FixStartStateCollision default_planner_request_adapters/FixStartStatePathConstraints""",
+            "start_state_max_bounds_error": 0.1,
+        },
+    }
+    ompl_planning_yaml = load_yaml(
+        "dummy_moveit", "config/ompl_planner.yaml"
+    )
+    planning_pipelines_config["ompl"].update(ompl_planning_yaml)
+
+    # Trajectory Execution Functionality
+    moveit_simple_controllers_yaml = load_yaml(
+        "dummy_moveit", "config/moveit_controllers.yaml"
+    )
+    moveit_controllers = {
+        "moveit_simple_controller_manager": moveit_simple_controllers_yaml,
+        "moveit_controller_manager": "moveit_simple_controller_manager/MoveItSimpleControllerManager",
+    }
+    trajectory_execution = {
+        "moveit_manage_controllers": True,
+        "trajectory_execution.allowed_execution_duration_scaling": 1.2,
+        "trajectory_execution.allowed_goal_duration_margin": 0.5,
+        "trajectory_execution.allowed_start_tolerance": 0.01,
+    }
+
+    planning_scene_monitor_parameters = {
+        "publish_planning_scene": True,
+        "publish_geometry_updates": True,
+        "publish_state_updates": True,
+        "publish_transforms_updates": True,
+    }
+
+    # Start the actual move_group node/action server
+    run_move_group_node = Node(
+        package="moveit_ros_move_group",
+        executable="move_group",
+        output="screen",
+        parameters=[
+            robot_description,
+            robot_description_semantic,
+            robot_description_kinematics,
+            robot_description_planning,
+            planning_pipelines_config,
+            trajectory_execution,
+            moveit_controllers,
+            planning_scene_monitor_parameters,
+        ],
+    )
+
+    # RViz
+    rviz_base = os.path.join(
+        get_package_share_directory("dummy_moveit"), "config"
+    )
+    rviz_full_config = os.path.join(rviz_base, "moveit.rviz")
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_full_config],
+        parameters=[
+            robot_description,
+            robot_description_semantic,
+            robot_description_planning,
+            robot_description_kinematics,
+            planning_pipelines_config,
+        ],
+    )
+
+    # Static TF
+    static_tf = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        name="static_transform_publisher",
+        output="log",
+        arguments=["0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "world", "base_link"],
+    )
+
+    can_serber = Node(
+        package="dummy2_can2eth",
+        executable="dummy2_can2eth_server",
+        name="dummy2_can2eth_server",
+        output="log",
+    )
+
+    # Publish TF
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+    )
+
+    # ros2_control using FakeSystem as hardware
+    ros2_controllers_path = os.path.join(
+        get_package_share_directory("dummy_moveit"),
+        "config",
+        "ros2_controllers.yaml",
+    )
+    ros2_control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[robot_description, ros2_controllers_path],
+        output="screen",
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "joint_state_broadcaster",
+            "--controller-manager",
+            "/controller_manager",
+        ],
+    )
+
+    prbt_arm_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "dummy_controller",
+            "--controller-manager",
+            "/controller_manager",
+        ],
+        output="screen",
+    )
+
+    # Warehouse mongodb server
+    #mongodb_server_node = Node(
+        #package="warehouse_ros_mongo",
+        #executable="mongo_wrapper_ros.py",
+        #parameters=[
+            #{"warehouse_port": 33829},
+            #{"warehouse_host": "localhost"},
+            #{"warehouse_plugin": "warehouse_ros_mongo::MongoDatabaseConnection"},
+       # ],
+      #  output="screen",
+    #)
+
+    return LaunchDescription(
+        [
+            #can_serber,
+            rviz_node,
+            static_tf,
+            robot_state_publisher,
+            run_move_group_node,
+            ros2_control_node,
+            joint_state_broadcaster_spawner,
+            prbt_arm_controller_spawner,
+            #mongodb_server_node,
+        ]
+    )
